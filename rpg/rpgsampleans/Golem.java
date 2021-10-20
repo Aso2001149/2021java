5e88964482aebeb890f0c94865a458e24fe283c9
Aso2001149 <83047073+Aso2001149@users.noreply.github.com>
2021-10-20 14:53:36 +0900

    Add files via upload

---
+++ rpg/rpgsampleans/Golem.java
@@ -0,0 +1,44 @@
+package rpgcreature;
+
+import java.util.Random;
+
+/**
+ * スライムクラス
+ */
+public class Golem extends Monster{
+    private final static int CRITICAL_HIT_RATE = 5;
+    /**
+     * スライムクラスのコンストラクタ
+     */
+    public Golem(){
+        super("ゴーレム",100);
+    }
+
+    /**
+     * 攻撃メソッド
+     * @param opponent：攻撃相手
+     */
+    @Override
+    public void attack(Creature opponent) {
+        Random r = new Random();
+        if(hp>0){
+            if( r.nextInt(100) < CRITICAL_HIT_RATE ){
+                //クリティカルヒット
+                int damage = 30;
+                System.out.printf("%sのクリティカルヒット！\n",getName());
+                opponent.damaged(damage);
+        
+                displayMessage(opponent,damage);
+            }
+                else{
+          int damage = r.nextInt(5)+5;
+        System.out.printf("%sの攻撃！\n",getName());
+                
+        opponent.damaged(damage);
+        
+        displayMessage(opponent,damage);
+                }
+        }
+    }
+    
+    }
